4e4dda36d67d9f9ba241aadcd084bab5272ac04a
Gaetano Sabino <gaetano.sabino@gmail.com>
2019-02-01 11:24:03 -0500

    cambios en saldo

--- mysql/pruebasdos.js
+++ mysql/pruebasdos.js
@@ -64,7 +64,7 @@ let prova1 = (callback) => {
                         res = {
                             id: cl.idcliente,
                             // saldo: Number((tot - iniz - patot).toFixed(2)),
-                            saldo: Number(tot - patot),
+                            saldo: Number((tot - patot).toFixed(2)),
                             comp_tot: Number(tot.toFixed(2)),
                             pagoTot: Number(patot.toFixed(2)),
                             inicial: Number(iniz.toFixed(2)),
