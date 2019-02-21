75f2b47e4b037a2b996402ce63c31b810d53b83b
Gaetano Sabino <gaetano.sabino@gmail.com>
2019-02-21 14:55:43 -0500

    cambios

--- server/routers/email/config.js
+++ server/routers/email/config.js
@@ -160,7 +160,7 @@ let email = (recibo, req, callback) => {
             <td>${recibo.fattura}</td>
             <td>${recibo.saldo}</td>
             <td>${recibo.cantidad}</td>
-            <td>${recibo.Saldo - recibo.cantidad}</td>
+            <td>${recibo.saldo - recibo.cantidad}</td>
         </tr>
     </table>
 </div>
